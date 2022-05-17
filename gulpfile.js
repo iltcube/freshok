4d1617d77f78636c227f64f8ca64330c3977e89f
iltcube <312.yakuba@gmail.com>
2022-05-17 07:12:40 +0300

    added top-slider section

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,18 @@
-const { src, dest, watch, parallel, series }  = require('gulp');
+const {
+    src,
+    dest,
+    watch,
+    parallel,
+    series
+} = require('gulp');
 
-const scss          = require('gulp-sass')(require('sass'));
-const browserSync   = require('browser-sync').create();
-const autoprefixer  = require('gulp-autoprefixer');
-const imagemin      = require('gulp-imagemin');
-const concat        = require('gulp-concat');
-const uglify        = require('gulp-uglify');
-const del           = require('del');
+const scss = require('gulp-sass')(require('sass'));
+const browserSync = require('browser-sync').create();
+const autoprefixer = require('gulp-autoprefixer');
+const imagemin = require('gulp-imagemin');
+const concat = require('gulp-concat');
+const uglify = require('gulp-uglify');
+const del = require('del');
 
 function browsersync() {
     browserSync.init({
@@ -17,9 +23,11 @@ function browsersync() {
     })
 }
 
-function styles () {
+function styles() {
     return src('app/scss/style.scss')
-        .pipe(scss({outputStyle: 'compressed'}))
+        .pipe(scss({
+            outputStyle: 'compressed'
+        }))
         .pipe(concat('style.min.css'))
         .pipe(autoprefixer({
             overrideBrowserslist: ['last 10 versions'],
@@ -31,38 +39,51 @@ function styles () {
 
 function scripts() {
     return src([
-        'node_modules/jquery/dist/jquery.js',
-        'app/js/main.js'
-    ])
-    .pipe(concat('main.min.js'))
-    .pipe(uglify())
-    .pipe(dest('app/js'))
-    .pipe(browserSync.stream())
+            'node_modules/jquery/dist/jquery.js',
+            'node_modules/slick-carousel/slick/slick.js',
+            'app/js/main.js'
+        ])
+        .pipe(concat('main.min.js'))
+        .pipe(uglify())
+        .pipe(dest('app/js'))
+        .pipe(browserSync.stream())
 }
 
 function images() {
     return src('app/images/**/*.*')
-    .pipe(imagemin([
-        imagemin.gifsicle({ interlaced: true }),
-        imagemin.mozjpeg({ quality: 75, progressive: true }),
-        imagemin.optipng({ optimizationLevel: 5 }),
-        imagemin.svgo({
-            plugins: [
-                { removeViewBox: true },
-                { cleanupIDs: false }
-            ]
-        })
-    ]))
-    .pipe(dest('dist/images'))
+        .pipe(imagemin([
+            imagemin.gifsicle({
+                interlaced: true
+            }),
+            imagemin.mozjpeg({
+                quality: 75,
+                progressive: true
+            }),
+            imagemin.optipng({
+                optimizationLevel: 5
+            }),
+            imagemin.svgo({
+                plugins: [{
+                        removeViewBox: true
+                    },
+                    {
+                        cleanupIDs: false
+                    }
+                ]
+            })
+        ]))
+        .pipe(dest('dist/images'))
 }
 
 function build() {
     return src([
-        'app/**/*.html',
-        'app/css/style.min.css',
-        'app/js/main.min.js'
-    ], {base: 'app'})
-    .pipe(dest('dist'))
+            'app/**/*.html',
+            'app/css/style.min.css',
+            'app/js/main.min.js'
+        ], {
+            base: 'app'
+        })
+        .pipe(dest('dist'))
 }
 
 function cleanDist() {
@@ -70,17 +91,17 @@ function cleanDist() {
 }
 
 function watching() {
-    watch (['app/scss/**/*.scss'], styles);
-    watch (['app/js/**/*.js', '!app/js/main.min.js'], scripts);
-    watch (['app/**/*.html']).on('change', browserSync.reload);
+    watch(['app/scss/**/*.scss'], styles);
+    watch(['app/js/**/*.js', '!app/js/main.min.js'], scripts);
+    watch(['app/**/*.html']).on('change', browserSync.reload);
 }
 
-exports.styles      = styles;
-exports.images      = images;
-exports.scripts     = scripts;
-exports.watching    = watching;
-exports.cleanDist   = cleanDist;
+exports.styles = styles;
+exports.images = images;
+exports.scripts = scripts;
+exports.watching = watching;
+exports.cleanDist = cleanDist;
 exports.browsersync = browsersync;
-exports.build       = series(cleanDist, images, build);
+exports.build = series(cleanDist, images, build);
 
 exports.default = parallel(styles, scripts, browsersync, watching);
