a5d21f3d7790f9d7134e503219b746fa0585cb5c
iltcube <312.yakuba@gmail.com>
2022-05-24 09:37:38 +0300

    added slider and advantages

--- app/js/main.js
+++ app/js/main.js
@@ -4,8 +4,8 @@ $(function () {
         prevArrow: '<button class="slick-btn slick-prev"><img src="images/icon/arrow-left.svg" alt="prev"></button>',
         nextArrow: '<button class="slick-btn slick-next"><img src="images/icon/arrow-right.svg" alt="prev"></button>',
         fade: true,
-        autoplay: true,
-        autoplaySpeed: 2000,
+        // autoplay: true,
+        // autoplaySpeed: 4000,
     });
 
 });
